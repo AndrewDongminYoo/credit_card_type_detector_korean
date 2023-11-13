79aea5fcc0b2758d4695868715b7c7ee8650404e
Dongmin Yu <ydm2790@gmail.com>
2023-11-13 15:13:22 +0900

    chore: 🎉 ::start with clean slate::
    Start developing packages with a clean slate thanks to the "Very Good CLI" tool!

---
+++ lib/src/credit_card_type_detector_korean.dart
@@ -0,0 +1,7 @@
+/// {@template credit_card_type_detector_korean}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class CreditCardTypeDetectorKorean {
+  /// {@macro credit_card_type_detector_korean}
+  const CreditCardTypeDetectorKorean();
+}
---
+++ test/src/credit_card_type_detector_korean_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:test/test.dart';
+import 'package:credit_card_type_detector_korean/credit_card_type_detector_korean.dart';
+
+void main() {
+  group('CreditCardTypeDetectorKorean', () {
+    test('can be instantiated', () {
+      expect(CreditCardTypeDetectorKorean(), isNotNull);
+    });
+  });
+}
